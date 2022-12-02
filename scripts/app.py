83a91c7dd80bd9c14e84e506d4ac546dea1ffecb
GaganNayyarHepta <119475857+GaganNayyarHepta@users.noreply.github.com>
2022-12-02 04:22:52 +0000

    test

--- scripts/app.py
+++ scripts/app.py
@@ -5,11 +5,11 @@ import streamlit as st
 from datetime import time
 
 from datetime import datetime
-import psycopg2
+#import psycopg2
 
-conn = psycopg2.connect("dbname='test_db' user='root' host='localhost' password='root'") 
+#conn = psycopg2.connect("dbname='test_db' user='root' host='localhost' password='root'") 
 
-cur = conn.cursor()
+#cur = conn.cursor()
 
 insert_query = """
 INSERT INTO streamlit_test1 (people_quality_score,
@@ -39,9 +39,9 @@ with st.form('cyber-security'):
     cyber_security = st.slider('Cyber Security', 0, 5, 1)
     cyber_security_comment = st.text_input("Comment")
     submit = st.form_submit_button(label='Submit')
-
+"""
 if submit:
     st.write(f"Score Submitted: {cyber_security}")
     cur.execute(insert_query,(People_Quality,people_quality_comment,cyber_security,cyber_security_comment))
     conn.commit()
-    cur.close()
+    cur.close()"""
